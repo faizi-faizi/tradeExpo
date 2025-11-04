4728a926dccce6623544e6e59ddedcdcabb574c7
faaizmohammed <faaizbymohammed@gmail.com>
2025-11-04 12:05:19 +0530

    some changes

--- server/server.js
+++ server/server.js
@@ -8,6 +8,9 @@ const router = require('./routes/userRoutes');
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.get("/",(req,res)=>[
+  res.json("Hello World")
+])
 
 // Connect DB
 dbConnection()
