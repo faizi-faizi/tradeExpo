e8df99a35de1976a741e65be12859d0c47ac4e35
faaizmohammed <faaizbymohammed@gmail.com>
2025-11-25 10:46:55 +0530

    card view feature in admin side

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -25,7 +25,7 @@ const addUser = async (req, res) => {
 
     const cardUrl = `${CLIENT_URL}/card/${newUser._id}`;
     newUser.cardUrl = cardUrl;
-    newUser.qr = await QRCode.toDataURL(cardUrl);
+    newUser.qr = await QRCode.toDataURL(cardUrl); 
 
     await newUser.save();
 
